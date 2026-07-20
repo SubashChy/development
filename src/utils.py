1033205840eba96915c9541b66a8a090bcd04a1c
Subash06 <subashchaudhary0000@gmail.com>
2026-07-20 13:26:46 +0545

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,9 @@ from src.exception import CustomException
 import dill
 from typing import List
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from typing import Any
+import pickle
 
 def save_object(file_path , obj):
     try:
@@ -20,11 +22,19 @@ def save_object(file_path , obj):
         raise CustomException(e,sys)
 
 
-def model_evaluate(X_train, X_test, y_train  , y_test , models) -> dict[str , dict[str , Any]]:
+def model_evaluate(X_train, X_test, y_train  , y_test , models , param) -> dict[str , dict[str , Any]]:
     try:
         report = {}
         for name , model in models.items():
+            hyperparameter = param.get(name)
+            logging.info(f"Paramter succesfully extracted {hyperparameter}")
+            gd = GridSearchCV( model , hyperparameter , cv = 3)
+            gd.fit(X_train , y_train)
+            
+            model.set_params(**gd.best_params_)
+            
             model.fit(X_train , y_train)
+            
             y_pred = model.predict(X_test)
             
             score = r2_score(y_test , y_pred)
@@ -37,3 +47,12 @@ def model_evaluate(X_train, X_test, y_train  , y_test , models) -> dict[str , di
         
     except Exception as e:
         raise CustomException(e, sys)  
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path , 'rb') as f:
+            return pickle.load(f)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
